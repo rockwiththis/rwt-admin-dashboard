a119a8f978c30840710623d0d3a4b60fe85f57bc
Evan Cook <evancookie2@gmail.com>
2018-12-09 21:06:33 -0800

    stub out songs post

--- components/UploadSongForm/index.js
+++ components/UploadSongForm/index.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 import './UploadSongForm.scss'
 
+const defaultFields = {
+  songTitle: '',
+  artistTitle: '',
+  youtubeUrl: '',
+  yearReleased: '',   // This field is not currently in the database
+}
+
 
 class UploadSongForm extends Component {
   constructor(props) {
   super(props)
   this.state = {
-    songTitle: '',
-    artistTitle: '',
-    youtubeUrl: '',
-    yearReleased: '',
+    fields: defaultFields
   }
 }
 
@@ -18,16 +22,80 @@ class UploadSongForm extends Component {
 //   console.log('>>> user', user)
 // }
 
-_handleInputChange = (inputName, event) => {
-  this.setState({ [inputName]: event.target.value })
+/* Current write schema
+ * const songsWriteSchema = {
+ *   name: { required: true },
+ *   description: { required: true },
+ *   imageUrl: { db: 'image_url', required: true },
+ *   curatorId: { db: 'curator_id' },
+ *   artistName: { db: 'artist_name', required: true},
+ *   spotify: {
+ *     required: true, // DB way ... shouldn't we actually at least one / exactly one link type?
+ *     fields: {
+ *       link: { required: true },
+ *     }
+ *   },
+ *   soundcloud: {
+ *     fields: {
+ *       link: { required: true },
+ *       trackId: { db: 'track_id', required: true },
+ *     }
+ *   },
+ *   youtube: {
+ *     fields: {
+ *       link: { required: true },
+ *       trackId: { db: 'track_id', required: true },
+ *     }
+ *   },
+ *   bpm: { required: true },
+ *   artistLocation: { db: 'artist_location', required: true }
+ */
+
+_handleInputChange = inputName => event => {
+  this.setState({
+    fields: {[inputName]: event.target.value }
+  });
 }
 
 _handleSubmit = (event) => {
   event.preventDefault();
-  const { songTitle, artistTitle, youtubeUrl, yearReleased } = this.state
-  // you'll have to make this postSong function
-  postSong({ songTitle, artistTitle, youtubeUrl, yearReleased })
-  // or just: postSong(this.state);
+
+  const request = new Request(
+    "http://localhost:9292/api/songs/", {
+    method: "POST",
+    body: JSON.stringify({
+      name: this.state.songTitle,
+      artistName: this.state.artistTitle,
+      youtube: {
+        link: this.state.youtubeUrl
+      }
+    })
+  });
+
+  fetch(request)
+    .then(response => {
+      if (response.ok) {
+        const msg = "Successfully added song"
+        console.log(msg);
+        refreshForm(msg)
+      } else {
+        console.log(`Server error: ${response.statusText}`);
+        this.setState({ error: response.statusText });
+      }
+      return;
+    })
+    .catch(error => {
+      console.log(error);
+      this.setState({ error: error });
+      return;
+    });
+}
+
+refreshForm = message => {
+  this.setState({
+    message: message,
+    fields: defaultFields
+  });
 }
 
 _onKeyPress = (event) => {
@@ -37,7 +105,7 @@ _onKeyPress = (event) => {
 }
 
 render() {
-  const inputFields = Object.keys(this.state);
+  const inputFields = Object.keys(this.state.fields);
   return (
     <div className={'loginForm__wrapper'} onKeyPress={this._onKeyPress}>
       {'upload song'}
@@ -59,6 +127,8 @@ render() {
           value={'submit'}
           className={'loginForm__submit-button'}
         />
+        {this.state.error && <p className='error'>{this.state.error}</p>}
+        {this.state.error && <p className='message'>{this.state.message}</p>}
       </form>
     </div>
   )
