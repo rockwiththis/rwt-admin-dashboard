9df22ab0fc82fe93bcec339872da0f7336244596
Dane Iliff <dane@MacBook-Pro-2.local>
2019-01-22 13:35:05 -0800

    Songs API URL is pulling 500 songs now

--- api/urls.js
+++ api/urls.js
@@ -1,3 +1,4 @@
-export const FETCH_ALL_SONGS_URL = 'http://localhost:9292/api/songs'
+// export const FETCH_ALL_SONGS_URL = 'http://localhost:9292/api/songs'
+export const FETCH_ALL_SONGS_URL = 'http://localhost:9292/api/songs?limit=500'
 export const FETCH_ALL_CURATORS_URL = 'http://localhost:9292/api/curators'
 export const FETCH_ALL_SUBGENRES_URL = 'http://localhost:9292/api/subgenres'
